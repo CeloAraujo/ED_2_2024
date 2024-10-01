a1a913eac57721dc9f83a4ae83e89b4179fd850c
CeloAraujo <celoparaujo99@gmail.com>
2024-10-01 11:33:02 -0300

    pop first double linked list

--- Aula1/DLL/DLL1.py
+++ Aula1/DLL/DLL1.py
@@ -36,6 +36,19 @@ class listaEncadeadaDupla:
             temp.prev = None
         self.length -= 1
         return temp
+    
+    def pop_first(self):
+        temp = self.head
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        self.length -= 1
+        return temp
 
 
 
